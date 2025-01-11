02ff6fc9bbc03dc7f857992ae9ffb3cb4890700f
Marcos <marcosjfr3@gmail.com>
2025-01-11 17:44:24 -0400

    checklist items added

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+    type Item = {
+        title: string;
+        checked?: boolean;
+    }
+    const GroupsItems: Item[] = [
+        { title: 'Visitar a la abuela', checked: false },
+        { title: 'Hacer la compra', checked: false },
+        { title: 'Estudiar Vue 3', checked: false },
+    ];
+</script>
+
+<template>
+    <ul>
+        <li :key="index" v-for="(item, index) in GroupsItems">
+            <ListItem :isChecked="item.checked">
+                {{ item.title }}
+            </ListItem>
+        </li> 
+    </ul>
+</template>
+
+<style scoped>
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+</style>
